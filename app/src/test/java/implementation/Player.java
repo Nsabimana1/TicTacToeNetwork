a9dd3efa80d87d083f979d267ccf2b01edea6e08
Nsabimana1 <nsabimanainnocent1@gmail.com>
2019-02-22 01:55:08 -0600

    added a player class

---
+++ app/src/test/java/implementation/Player.java
@@ -0,0 +1,10 @@
+package implementation;
+
+public class Player {
+    int ipAddress;
+    int port;
+    public Player(int ipAddress, int port){
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+}
