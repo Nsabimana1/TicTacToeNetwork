7e038129349bdd3257c19444cc5204af97142730
ADadBadJoke <bellavery99@gmail.com>
2019-02-26 17:05:18 -0600

    added comment for showing toast once connection is attempted from another device

--- app/src/main/java/com/example/myapplication/OpeningActivity.java
+++ app/src/main/java/com/example/myapplication/OpeningActivity.java
@@ -1,15 +1,20 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class OpeningActivity extends AppCompatActivity {
 
     private Button connect;
     private Button enterGame;
+    private TextView userIPAddress;
+    private TextView otherIPAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +27,27 @@ public class OpeningActivity extends AppCompatActivity {
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent NewRecipeIntent = new Intent(OpeningActivity.this, JoiningGameActivity.class);
-                startActivity(NewRecipeIntent);
+
             }
         } );
 
         enterGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ShoppingListIntent = new Intent(OpeningActivity.this, HostingGameActivity.class);
-                startActivity(ShoppingListIntent);
+
             }
         } );
     }
+
+    public void displayToast(String message){
+        Context context = getApplicationContext();
+        CharSequence text = message;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    // if (connection is attempted from another device) {
+    // displayToast("connection has been attempted from " + otherIPAddress);
+    // }
 }
