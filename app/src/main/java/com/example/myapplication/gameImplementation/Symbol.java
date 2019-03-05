0126139d8d48ffeef19aa68f1f0b288bbe9395cb
theDrunkenManatee <theDrunkenManatee@users.noreply.github.com>
2019-03-04 19:21:34 -0600

    Update Symbol.java

--- app/src/main/java/com/example/myapplication/gameImplementation/Symbol.java
+++ app/src/main/java/com/example/myapplication/gameImplementation/Symbol.java
@@ -6,16 +6,23 @@ public enum Symbol {
         public String toString() {
             return "X";
         }
+        @Override
+        public WinState getWinner() {return WinState.X_WIN;}
     }, O {
         @Override
         public String toString() {
             return "O";
         }
+        @Override
+        public WinState getWinner() {return WinState.O_WIN;}
     }, BLANK {
         @Override
         public String toString() {
             return " ";
         }
+        @Override
+        public WinState getWinner() {return WinState.NO_WIN;}
     };
     abstract public String toString();
+    abstract public WinState getWinner();
 }
