4e52e46eea4165b2dd938c3420b2ef20ec81dc03
theDrunkenManatee <theDrunkenManatee@users.noreply.github.com>
2019-03-05 16:55:15 -0600

    WinState checking implemented

--- app/src/main/java/com/example/myapplication/gameImplementation/TicTacToeGame.java
+++ app/src/main/java/com/example/myapplication/gameImplementation/TicTacToeGame.java
@@ -29,15 +29,19 @@ public class TicTacToeGame {
 
     public boolean makeMove(Move move) {
         boolean moveMade;
-        if(move.toString().equals(recentMove.toString())) {
-            moveMade =  false;
-        } else {
-            if(board.makeMove(move)) {
-                recentMove = move;
-                moveMade =  true;
+        if(checkWin()==WinState.NO_WIN) {
+            if(move.toString().equals(recentMove.toString())) {
+                moveMade =  false;
             } else {
-                moveMade = false;
+                if(board.makeMove(move)) {
+                    recentMove = move;
+                    moveMade =  true;
+                } else {
+                    moveMade = false;
+                }
             }
+        } else {
+            moveMade = false;
         }
         return moveMade;
     }
