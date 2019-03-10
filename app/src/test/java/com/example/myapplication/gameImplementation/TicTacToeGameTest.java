493e32d1c13d82c569d98ae66a6b0fa5dc29f53c
theDrunkenManatee <samorege@hendrix.edu>
2019-03-09 21:10:29 -0600

    Adding more gameTesting cases

--- app/src/test/java/com/example/myapplication/gameImplementation/TicTacToeGameTest.java
+++ app/src/test/java/com/example/myapplication/gameImplementation/TicTacToeGameTest.java
@@ -5,24 +5,29 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TicTacToeGameTest {
-    
 
+    //Todo
     @Test
     public void getMoveString() {
     }
 
+
+    //TODO
     @Test
     public void parseMoveString() {
     }
 
+    //TODO
     @Test
     public void makeMove() {
     }
 
+    //TODO
     @Test
     public void checkWin() {
     }
 
+    //TODO
     @Test
     public void getRecentMove() {
     }
