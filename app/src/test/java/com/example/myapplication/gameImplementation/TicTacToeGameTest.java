d784c48f49977a6e6fcb2f21f78649ed92759a40
theDrunkenManatee <samorege@hendrix.edu>
2019-03-09 21:09:50 -0600

    Adding game testing cases

---
+++ app/src/test/java/com/example/myapplication/gameImplementation/TicTacToeGameTest.java
@@ -0,0 +1,29 @@
+package com.example.myapplication.gameImplementation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TicTacToeGameTest {
+    
+
+    @Test
+    public void getMoveString() {
+    }
+
+    @Test
+    public void parseMoveString() {
+    }
+
+    @Test
+    public void makeMove() {
+    }
+
+    @Test
+    public void checkWin() {
+    }
+
+    @Test
+    public void getRecentMove() {
+    }
+}
